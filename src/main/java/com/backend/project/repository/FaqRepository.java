c177fc00574111730e4b29caedfc802a1a2cff27
Catarinak1 <catarinakaucher@gmail.com>
2025-01-20 21:01:17 +0100

    Created FaqControllerIntegrationTest

--- src/main/java/com/backend/project/repository/FaqRepository.java
+++ src/main/java/com/backend/project/repository/FaqRepository.java
@@ -4,6 +4,12 @@ package com.backend.project.repository;
 import com.backend.project.model.Faq;
 import org.springframework.data.mongodb.repository.MongoRepository;
 
+import java.util.Optional;
+
 
 public interface FaqRepository extends MongoRepository<Faq, String> {
+    Iterable<? extends Faq> findByQuestionContaining(String s);
+
+
+    Optional<Faq> findByQuestion(String s);
 }
